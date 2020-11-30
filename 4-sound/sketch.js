2f2f7fedfcc4253ffd464c31259d93035217befb
t0mmyry4n <74562524+t0mmyry4n@users.noreply.github.com>
2020-11-30 10:43:48 -0800

    troubleshooting

--- 4-sound/sketch.js
+++ 4-sound/sketch.js
@@ -1,115 +1,90 @@
-//create an empty array called balls
-let balls = [];
+var airplane
+var ocean
+var seagull
+var walking
+var wind
+var planeimage
+var seagullimage
 
-//create a variable to hold your avatar
-let me;
-
-let mySound;
-
-function preload() {
-  soundFormats('mp3', 'ogg');
-  mySound = loadSound('boing1.mp3');
+function preload(){
+  airplane = loadSound("airplane.mp3")
+  ocean = loadSound("ocean.mp3")
+  seagull = loadSound("seagull.mp3")
+  walking = loadSound("walking.mp3")
+  wind = loadSound("wind.mp3")
+  planeimage = loadImage("plane.jpg")
+  seagullimage = loadImage("seagull.jpg")
+}
+function waves(){
+  stroke("blue")
+  beginShape()
+  curveVertex(0,200)
+  curveVertex(20,190)
+  curveVertex(40,200)
+  curveVertex(40,190)
+  curveVertex(60,200)
+  curveVertex(80,190)
+  curveVertex(100,200)
+  curveVertex(120,190)
+  curveVertex(140,200)
+  curveVertex(160,190)
+  curveVertex(180,200)
+  curveVertex(200,190)
+  curveVertex(220,200)
+  curveVertex(240,190)
+  curveVertex(260,200)
+  curveVertex(280,190)
+  curveVertex(300,200)
+  curveVertex(320,190)
+  curveVertex(340,200)
+  curveVertex(360,190)
+  curveVertex(380,200)
+  curveVertex(400,190)
+  endShape()
+}
+function ground(){
+  fill("brown")
+  rect(0,275,400,275)
+}
+function air(x,y){
+  stroke("white")
+  beginShape()
+  curveVertex(x,y)
+  curveVertex(x+5,y)
+  curveVertex(x+10,y+5)
+  curveVertex(x+15,y)
+  curveVertex(x+10,y-5)
+  curveVertex(x+7.5,y)
+  endShape()
 }
 
-function setup() {
-  createCanvas(500, 400);
-
-  //make one avatar called me
-  me = new Avatar(width/2, 300, 3);
-
+function setup(){
+createCanvas(400,400)
 }
 
 function draw(){
-	background(220);
-
-  me.drawMe();
-  me.moveMe();
-
-  if (frameCount % 25 == 0) {
-      let  b = new Ball(width, random(0,height), -3);
-      balls.push(b);
-      console.log(balls); //print the balls array to the console
-    }
-
-//	draw all the balls in that array
-	for (let i = 0; i < balls.length; i++) {
-	 	      balls[i].drawBall();
-       	  balls[i].moveBall();
-        	balls[i].bounceBall();
-	  }
-
+  text('press 1-5',200,20)
+  background("blue")
+  image(planeimage,50,50,40,40)
+  image(seagullimage,370,50,20,20)
+  waves()
+  ground()
+  air(200,170)
+  air(190,180)
+  air(210,190)
 }
+function keyTyped(){
 
-//avatar class
-class Avatar {
-
-	constructor(x,y, speed){ //every avatar needs an x value, a y value, and a speed
-		    this.x = x;
-    		this.y = y;
-        this.speed = speed;
-	}
-
-	drawMe(){  // draw the running person
-    		stroke("green");
-        strokeWeight(3);
-    		fill("blue");
-		    ellipse(this.x,this.y,20,20);
-        line(this.x,this.y, this.x, this.y+40);
-        line(this.x, this.y+40, this.x-20, this.y+60);
-        line(this.x, this.y+40, this.x+10, this.y+50);
-        line(this.x+10, this.y+50, this.x+5, this.y+60);
-        line(this.x, this.y+15, this.x-10, this.y+25);
-        line(this.x-10, this.y+25, this.x+10, this.y+35);
-	}
-
-	moveMe(){
-    if (keyIsDown(UP_ARROW)) { //if you hold the up arrow, move up by speed
-       this.y -= this.speed;
-    }
-
-    if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by speed
-        this.y += this.speed;
-    }
-	}
-
-  die(){
-
-  }
-
+if (key === '1'){
+  airplane()
+} else if(key==='2'){
+  ocean()
+}else if(key==='3'){
+  seagull()
+}else if(key==='4'){
+  walking()
+}else(key==='5'){
+  wind()
 }
-
-
-//ball class from which to create new balls with similar properties.
-class Ball {
-
-	//every ball needs an x value, a y value, and a speed
-	constructor(x,y, speed){
-		this.x = x;
-		this.y = y;
-  	this.speed = speed;
-	}
-
-	// draw a ball on the screen at x,y
-	drawBall(){
-		stroke(0);
-  	strokeWeight(1);
-		fill("red");
-		ellipse(this.x,this.y,10,10);
-	}
-
-	//update the location of the ball, so it moves across the screen
-	moveBall(){
-		this.x = this.x+ this.speed;
-		this.y = this.y+.5;
-	}
-
-	//if the ball hits the paddle, change the speed value to negative (send it in the opposite direction)
-  	bounceBall(){
-    		if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+40){
-      			this.speed = -this.speed;
-            mySound.setVolume(0.1);
-            mySound.play();
-    		}
-  	}
 
 }
